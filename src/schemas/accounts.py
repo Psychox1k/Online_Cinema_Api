e6995a3b7d205c9886e46c9428314a6adce02520
Psychox1k <success01001011@gmail.com>
2026-05-21 17:37:16 +0200

    feat: add accounts and profiles schemas

---
+++ src/schemas/accounts.py
@@ -0,0 +1,72 @@
+from pydantic import BaseModel, EmailStr, field_validator
+
+from database import accounts_validators
+from database.validators.accounts import validate_password_strength
+
+
+class BaseEmailPasswordSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = {
+        "from_attributes": True
+    }
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value):
+        return accounts_validators.validate_email(value)
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        return accounts_validators.validate_password_strength(value)
+
+
+class UserRegistrationRequestSchema(BaseEmailPasswordSchema):
+    pass
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+class PasswordResetCompleteSchema(BaseEmailPasswordSchema):
+    token: str
+
+class UserLoginRequestSchema(BaseEmailPasswordSchema):
+    pass
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+class MessageResponseSchema(BaseModel):
+    message: str
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class PasswordChangeRequestSchema(BaseModel):
+    old_password: str
+    new_password: str
+
+    @field_validator("new_password")
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        return accounts_validators.validate_password_strength(value)
+
---
+++ src/schemas/profiles.py
@@ -0,0 +1,96 @@
+from datetime import date
+
+from fastapi import UploadFile, Form, File, HTTPException
+from pydantic import BaseModel, field_validator, HttpUrl
+
+from validation import (
+    validate_name,
+    validate_image,
+    validate_gender,
+    validate_birth_date
+)
+
+class ProfileCreateSchema(BaseModel):
+    first_name: str
+    last_name: str
+    gender: str
+    date_of_birth: date
+    info: str
+    avatar: UploadFile
+
+    @field_validator("first_name", "last_name")
+    @classmethod
+    def validate_name_field(cls, name: str) -> str:
+        try:
+            validate_name(name)
+            return name.lower()
+        except ValueError as e:
+            raise HTTPException(
+                status_code=422,
+                detail=[{
+                    "type": "value_error",
+                    "loc": ["first_name" if "first_name" in name else "last_name"],
+                    "msg": str(e),
+                    "input": name
+                }]
+            )
+    
+
+    @field_validator("gender")
+    @classmethod
+    def validate_gender(cls, gender: str) -> str:
+        try:
+            validate_gender(gender)
+            return gender
+        except ValueError as e:
+            raise HTTPException(
+                status_code=422,
+                detail=[{
+                    "type": "value_error",
+                    "loc": ["gender"],
+                    "msg": str(e),
+                    "input": gender
+                }]
+            )
+    @field_validator("date_of_birth")
+    @classmethod
+    def validate_date_of_birth(cls, date_of_birth: date) -> date:
+        try:
+            validate_birth_date(date_of_birth)
+            return date_of_birth
+        except ValueError as e:
+            raise HTTPException(
+                status_code=422,
+                detail=[{
+                    "type": "value_error",
+                    "loc": ["date_of_birth"],
+                    "msg": str(e),
+                    "input": date_of_birth
+                }]
+            )
+
+    @field_validator("info")
+    @classmethod
+    def validate_info(cls, info: str) -> str:
+        cleaned_info = info.strip()
+        if not cleaned_info:
+            raise HTTPException(
+                status_code=422,
+                detail=[{
+                    "type": "value_error",
+                    "loc": ["info"],
+                    "msg": "Info field cannot be empty or contain only spaces.",
+                    "input": info
+                }]
+            )
+
+
+class ProfileResponseSchema(BaseModel):
+    id: int
+    user_id: int
+    first_name: str
+    last_name: str
+    gender: str
+    date_of_birth: date
+    info: str
+    avatar: HttpUrl
