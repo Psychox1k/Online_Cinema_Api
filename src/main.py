04b57a7dbd49deffef4d54c0e91a54525773a33e
Psychox1k <success01001011@gmail.com>
2026-05-22 17:40:33 +0200

    feat: register accounts and profiles routers in main app

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
-
+from routes import accounts_router
+from routes import profiles_router
 app = FastAPI(title="Online Cinema API")
+
+
+api_version_prefix = "/api/v1"
+
+app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
+app.include_router(profiles_router, prefix=f"{api_version_prefix}/profiles", tags=["profiles"])
 
 @app.get("/")
 def read_root():
