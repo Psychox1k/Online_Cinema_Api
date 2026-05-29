2cac6b18ce6e91c46bc5fa69b94eeb21b5cfa0f6
Psychox1k <success01001011@gmail.com>
2026-05-29 15:29:30 +0200

    feat(routes): add routes for notifications

---
+++ src/routes/notifications.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, status
+from fastapi.params import Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from config.dependencies import get_current_moderator
+from database import get_db, UserModel, NotificationModel
+from schemas import NotificationSchema
+
+router = APIRouter()
+
+@router.get(
+    "/",
+    response_model=list[NotificationSchema],
+    status_code=status.HTTP_200_OK,
+    summary="Get user notifications",
+    description="Retrieve a list of all notifications for the currently authenticated moderator or admin. Sorted by newest first."
+)
+async def get_user_notifcations(
+    db: AsyncSession = Depends(get_db),
+    current_user: UserModel = Depends(get_current_moderator)
+) -> NotificationSchema:
+    stmt = select(NotificationModel).where(
+        NotificationModel.user_id == current_user.id
+    ).order_by(NotificationModel.created_at.desc())
+
+    result = await db.execute(stmt)
+    notifications = result.scalars().all()
+
+    return notifications
